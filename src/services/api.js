c980aa01253899783ad247692ee78054cb6eb689
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-29 12:33:07 -0300

    Refactor: changed path wrap signal

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from axios;
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://api.spaceflightnewsapi.net/v3"
+    baseURL: `https://api.spaceflightnewsapi.net/v3`
 })
 
 export default api;
