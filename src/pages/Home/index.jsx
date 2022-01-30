1d0f8aaa762210a63b6581a216bc781159cc29d2
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-30 17:51:52 -0300

    Refactor: changed code to display cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,18 @@
+import { Button } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
 
+    const navigate = useNavigate()
 
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return (
+    <main>
+      <h1>HOME</h1>
+      <Button onClick={() => navigate('/dashboard')} variant="contained">
+        Dashboard
+      </Button>
+    </main>
+  );
+};
 
 export default Home;
