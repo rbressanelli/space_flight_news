f585fca712c4f63639be584eedb9a4991d6e17b6
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-30 17:54:40 -0300

    Refactor: changed css base from index to app

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Providers from "./contexts";
-import {CssBaseline} from '@material-ui/core';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode>    
     <BrowserRouter>
       <Providers>
-      <CssBaseline />
         <App />
       </Providers>
     </BrowserRouter>
