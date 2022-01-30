ac92fb0a19bbf9b72d4291971fe3d2d6f4acf235
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-30 17:53:30 -0300

    Refactor: changed wrap card element

--- src/components/FlightNewsList/index.jsx
+++ src/components/FlightNewsList/index.jsx
@@ -1,5 +1,7 @@
+import { Container, Paper } from "@material-ui/core";
 import { useFlightNews } from "../../contexts/flightnews";
 import FlightNewsCard from "../FlightNewsCard";
+import {style} from './styles'
 
 const FlightNewsList = () => {
   const { flightList } = useFlightNews();
@@ -8,9 +10,9 @@ const FlightNewsList = () => {
     <>
       {flightList &&
         flightList.map((news) => (
-          <div key={news.id.toString()}>
+          <Container sx={style} key={news.id.toString()} >
             <FlightNewsCard flightNews={news} />
-          </div>
+          </Container>
         ))}
     </>
   );
