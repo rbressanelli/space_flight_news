9be2ffe5b0abe258ac858de4dce35b03a709e094
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-31 01:45:34 -0300

    Feat: added modal component

---
+++ src/components/NewsModal/styles.js
@@ -0,0 +1,19 @@
+export const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: {
+    mobile: 300,
+    tablet: 600,
+    laptop: 800,
+  }, 
+  bgcolor: "background.paper",
+  border: "2px solid #302E53",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: {
+    xs: 1,
+    sm: 3,
+  },
+};
