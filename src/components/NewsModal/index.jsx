291443d48634a234fbf5ed510beb58b33d3b2a3b
Roberto Bressanelli <rbressanelli@gmail.com>
2022-01-31 08:23:53 -0300

    Feat: added fade effect to modal

--- src/components/NewsModal/index.jsx
+++ src/components/NewsModal/index.jsx
@@ -1,20 +1,27 @@
-import { Box, Modal } from "@material-ui/core";
+import { Box, Fade, Modal } from "@material-ui/core";
 import FlightNewsCard from "../FlightNewsCard";
+import { Backdrop } from "@material-ui/core";
 import { style } from "./styles";
 
 const NewsModal = ({ open, data, setOpen }) => {
-
   const handleClose = () => setOpen(false);
 
   return (
     <>
       <Modal
+        closeAfterTransition
         open={open}
-        onClose={handleClose}        
+        onClose={handleClose}
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 1000,
+        }}
       >
-        <Box sx={style}>
-          <FlightNewsCard flightNews={data} isOpen={open}/>
-        </Box>
+        <Fade in={open}>
+          <Box sx={style}>
+            <FlightNewsCard flightNews={data} isOpen={open} />
+          </Box>
+        </Fade>
       </Modal>
     </>
   );
