da59dba35ae2493c8750e23b3fc2d08d022c8a3c
Roberto Bressanelli <rbressanelli@gmail.com>
2022-02-02 22:13:59 -0300

    Feat: added background video

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,34 @@
-import { Button, Grid } from "@material-ui/core";
+import { Box, Button, Fade, Grid, Paper } from "@material-ui/core";
 import { useNavigate } from "react-router-dom";
 import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import Video from "../../assets/backgroundVideo.mp4";
+import Zoom from "@mui/material/Zoom";
 
-import { style } from "./styles";
+import { style, VideoBackGround, buttonStyle } from "./styles";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <Grid container sx={style}>
-      <Grid
-        item
-        xs={12}
-        sx={{
-          minWidth: 200,
-        }}
-      >
-        <Button
-          onClick={() => navigate("/dashboard")}
-          variant="contained"
-          fullWidth
-          endIcon={<RocketLaunchIcon />}
-        >
-          Enter
-        </Button>
-      </Grid>
-    </Grid>
+    <>
+      <Zoom in timeout={2500}>
+        <Grid container sx={style}>
+          <Grid item mobile={12}>
+            <VideoBackGround autoPlay loop muted id="video">
+              <source src={Video} type="video/mp4" />
+            </VideoBackGround>
+            <Button
+              sx={buttonStyle}
+              onClick={() => navigate("/dashboard")}
+              variant="contained"
+              endIcon={<RocketLaunchIcon />}
+            >
+              Enter
+            </Button>
+          </Grid>
+        </Grid>
+      </Zoom>
+    </>
   );
 };
 
